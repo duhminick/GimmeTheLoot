8f245b59dfec625f42bf0ac7bf74f4a3eda65295
Dominic Sciascia <dominic@duhminick.com>
2019-12-28 03:06:58 -0500

    Added reddit to monitor service

--- monitor/reddit.go
+++ monitor/reddit.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"log"
+	"strings"
+	"time"
+
+	snoo "github.com/turnage/graw/reddit"
+)
+
 // reddit is the fetching service for reddit.com
 type reddit struct{}
 
@@ -8,5 +16,29 @@ func (f reddit) ShouldFetch(monitor Monitor) bool {
 }
 
 func (f reddit) GetItems(monitor Monitor) []Item {
-	return []Item{}
+	var items []Item
+
+	subreddit := strings.Split(monitor.URL, "reddit.com")[1]
+
+	handle, err := snoo.NewScript("GimmeTheLoot", 5*time.Second)
+	if err != nil {
+		log.Printf("Error! %s", err)
+	}
+
+	harvest, err := handle.Listing(subreddit, "")
+	if err != nil {
+		log.Printf("Error! %s", err)
+	}
+
+	for _, post := range harvest.Posts {
+		if len(monitor.Keywords) == 0 || monitor.KeywordInWord(post.Title) {
+			items = append(items, Item{
+				Name:   post.Title,
+				URL:    post.Permalink,
+				Source: "reddit",
+			})
+		}
+	}
+
+	return items
 }
