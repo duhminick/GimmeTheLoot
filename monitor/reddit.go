acc7869e7430a69215778e88078b9dfd1e613be5
Dominic Sciascia <dominic@duhminick.com>
2020-12-06 03:39:24 -0500

    Getting price from reddit titles

--- monitor/reddit.go
+++ monitor/reddit.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -13,6 +16,23 @@ type reddit struct{}
 
 func (f reddit) ShouldFetch(monitor Monitor) bool {
 	return monitor.Type == "reddit"
+}
+
+func getPriceFromTitle(title string) (string, error) {
+	re := regexp.MustCompile(`\=\s*\$?(\d+\.?\d+)|\$(\d+\.?\d+)`)
+	results := re.FindStringSubmatch(title)
+	if len(results) == 0 {
+		return "", fmt.Errorf("could not find a price")
+	}
+
+	// There are two capture groups in the regular expression
+	// if the 1st match is there, then pick this one over the
+	// 2nd match
+	if results[1] != "" {
+		return results[1], nil
+	}
+
+	return results[len(results)-1], nil
 }
 
 func (f reddit) GetItems(monitor Monitor) []Item {
@@ -32,11 +52,18 @@ func (f reddit) GetItems(monitor Monitor) []Item {
 
 	for _, post := range harvest.Posts {
 		if len(monitor.Keywords) == 0 || monitor.KeywordInWord(post.Title) {
-			items = append(items, Item{
+			newItem := Item{
 				Name:   post.Title,
 				URL:    "https://reddit.com" + post.Permalink,
 				Source: "reddit",
-			})
+			}
+
+			if price, err := getPriceFromTitle(post.Title); err == nil {
+				priceFloat, _ := strconv.ParseFloat(price, 32)
+				newItem.Price = float32(priceFloat)
+			}
+
+			items = append(items, newItem)
 		}
 	}
 
